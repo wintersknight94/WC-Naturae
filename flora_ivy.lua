7bada8705d2617b65953ffb239311becead4e445
Wyntr Stardust <62315647+wintersknight94@users.noreply.github.com>
2024-09-23 22:12:03 -0500

    changed ivy drawtype, removed ivy_vine node

--- flora_ivy.lua
+++ flora_ivy.lua
@@ -10,55 +10,31 @@ local vine = modname.. "_ivy_vine.png"
 ------------------------------------------------------------------------
 minetest.register_node(modname.. ":ivy", {
 	description = "Ivy",
-	drawtype = 'plantlike',
+--	drawtype = 'plantlike',
+	drawtype = 'firelike',
 	waving = 1,
-	tiles = {sprout},
+	tiles = {vine}, --{sprout},
 	inventory_image = ivy,
 	wield_image = ivy,
 	sunlight_propagates = true,
 	paramtype = 'light',
-	paramtype2 = "meshoptions",
-	place_param2 = 4,
+--	paramtype2 = "meshoptions",
+--	place_param2 = 4,
 	walkable = false,
 	groups = {
 		snappy = 1,
 		ivy = 1,
 		flora = 1,
 		flammable = 2,
-		attached_node = 1,
-		stack_as_node = 1,
+--		attached_node = 1,
+--		stack_as_node = 1,
 		peat_grindable_item = 1,
-		[modname .. "_spread"] = 1
+--		[modname .. "_spread"] = 1
 	},
 	sounds = nodecore.sounds("nc_terrain_swishy"),
 	buildable_to = true,
 })
 ------------------------------------------------------------------------
-minetest.register_node(modname.. ":ivy_vine", {
-	description = "Ivy",
-	drawtype = 'signlike',
-	tiles = {vine},
-	inventory_image = ivy,
-	wield_image = ivy,
-	sunlight_propagates = true,
-	paramtype = 'light',
-	paramtype2 = 'wallmounted',
-	walkable = false,
-	groups = {
-		snappy = 1,
-		ivy = 1,
-		flora = 1,
-		flammable = 2,
-		attached_node = 1,
-	},
-	climbable = true,
-	sounds = nodecore.sounds("nc_terrain_swishy"),
-	selection_box = {
-		type = "wallmounted",
-	},
-	buildable_to = true,
-	drop = modname.. ":ivy"
-})
 -- ================================================================== --
 minetest.register_decoration({
 	label = {modname .. ":ivy"},
