78d44af78d770bad124bac16710e7ea9f23407c3
Wyntr Stardust <62315647+wintersknight94@users.noreply.github.com>
2024-10-06 20:16:58 -0500

    new giant honeycap generation

--- decor_bigmushroom.lua
+++ decor_bigmushroom.lua
@@ -20,3 +20,18 @@ minetest.register_decoration({
 		replacements = {},
 		place_offset_y = -1
 	})
+
+minetest.register_decoration({
+		deco_type = "schematic",
+		place_on = {"group:soil", "group:mud"},
+		sidelen = 16,
+		fill_ratio = 0.00002,
+		biomes = {"mudflat", "floodland", "rainforest", "old_forest", "ancient_forest"},
+		y_min = -64,
+		y_max = 64,
+		schematic = nodecore.bigmushroom_honey_schematic,
+		flags = "place_center_x, place_center_z",
+		rotation = "random",
+		replacements = {},
+		place_offset_y = -1
+	})
