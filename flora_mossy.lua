306e94102b2f42d11f4a956628dff01982d4b173
Wyntr Stardust <62315647+wintersknight94@users.noreply.github.com>
2024-10-13 08:29:24 -0500

    updated moss mechanics

--- flora_mossy.lua
+++ flora_mossy.lua
@@ -1,6 +1,6 @@
 -- LUALOCALS < ---------------------------------------------------------
-local math, minetest, nodecore, pairs, type, vector
-    = math, minetest, nodecore, pairs, type, vector
+local math, minetest, nodecore, pairs, tonumber, type, vector
+    = math, minetest, nodecore, pairs, tonumber, type, vector
 local math_random
     = math.random
 -- LUALOCALS > ---------------------------------------------------------
@@ -39,6 +39,14 @@ local function groupswith(olddef, added)
 	end
 	return groups
 end
+local function blend(color, r, g, b)
+	if (not color) or (color.a == 0) then return color end
+	return {
+		r = (color.r + r) / 2,
+		g = (color.g + g) / 2,
+		b = (color.b + b) / 2,
+	}
+end
 local function register_mossy(subname, fromname)
 	local olddef = minetest.registered_nodes[fromname]
 	if not olddef then return end
@@ -48,7 +56,10 @@ local function register_mossy(subname, fromname)
 			tiles = tileswith(olddef, "mossy"),
 			drop_in_place = fromname,
 			groups = groupswith(olddef, "mossy"),
-			sounds = nodecore.sounds("nc_terrain_crunchy")
+			sounds = nodecore.sounds("nc_terrain_crunchy"),
+			paramtype = olddef.paramtype,
+			paramtype2 = olddef.paramtype2,
+			mapcolor = blend(olddef.mapcolor, 62, 101, 27),
 		})
 	local dyingname = modname .. ":mossy_" .. subname .. "_dying"
 	minetest.register_node(dyingname, {
@@ -56,7 +67,10 @@ local function register_mossy(subname, fromname)
 			tiles = tileswith(olddef, "mossy_dying"),
 			drop_in_place = fromname,
 			groups = groupswith(olddef, "mossy_dying"),
-			sounds = nodecore.sounds("nc_terrain_crunchy")
+			sounds = nodecore.sounds("nc_terrain_crunchy"),
+			paramtype = olddef.paramtype,
+			paramtype2 = olddef.paramtype2,
+			mapcolor = blend(olddef.mapcolor, 54, 41, 28),
 		})
 	local mossgroup = {
 		original = fromname,
@@ -75,6 +89,7 @@ register_mossy("stone", "nc_terrain:stone")
 register_mossy("thatch", minetest.registered_nodes["nc_flora:thatch"]
 	and "nc_flora:thatch" or modname .. ":thatch")
 register_mossy("trunk", "nc_tree:tree")
+register_mossy("stump", "nc_tree:root")
 register_mossy("dirt", "nc_terrain:dirt")
 register_mossy("bricks", "nc_stonework:bricks_stone")
 register_mossy("bricks_bonded", "nc_stonework:bricks_stone_bonded")
@@ -87,6 +102,9 @@ register_mossy("sandstone_bricks_bonded", "nc_stonework:bricks_sandstone_bonded"
 for i = 1, 7 do
 	register_mossy("hstone" .. i, "nc_terrain:hard_stone_" .. i)
 end
+register_mossy("lodestone", "nc_lode:stone")
+register_mossy("gravel", "nc_terrain:gravel")
+register_mossy("decaylog", modname .. ":decayed_log")
 
 ------------------------------------------------------------------------
 -- SPREADING ABM
@@ -102,7 +120,8 @@ local breathable_drawtypes = {
 	raillike = true,
 	nodebox = true,
 	mesh = true,
-	plantlike_rooted = true
+	plantlike_rooted = true,
+	flowingliquid = true
 }
 
 local breathable_nodes = {}
@@ -131,9 +150,20 @@ local function canbreathe(pos)
 		local p = vector.add(pos, alldirs[i])
 		local n = minetest.get_node(p)
 		if breathable_nodes[n.name] then
-			return true
+			local l = nodecore.get_node_light(p);
+			if l and l > 0 then
+				return true
+			end
 		end
 	end
+end
+
+local function setnodename(pos, oldnode, newname)
+	return minetest.set_node(pos, {
+			name = newname,
+			param = oldnode.param,
+			param2 = oldnode.param2,
+		})
 end
 
 minetest.register_abm({
@@ -143,10 +173,10 @@ minetest.register_abm({
 		chance = 10,
 		action = function(pos, node)
 			if not loaded(pos) then return end
-			if not canbreathe(pos) then
+			if not canbreathe(pos) or #nodecore.find_nodes_around(pos, "group:mossy", 1) > 7 then
 				local grp = mossy_groups[node.name]
 				return grp and grp.original
-				and nodecore.set_node(pos, {name = grp.original})
+				and setnodename(pos, node, grp.original)
 			end
 			local topos = {
 				x = pos.x + math_random(-1, 1),
@@ -156,13 +186,13 @@ minetest.register_abm({
 			local tonode = minetest.get_node(topos)
 			if minetest.get_item_group(tonode.name, "lux_emit") > 0 then
 				local grp = mossy_groups[node.name]
-				return grp and grp.original
-				and nodecore.set_node(pos, {name = grp.dying})
+				return grp and grp.dying
+				and setnodename(pos, node, grp.dying)
 			else
 				local grp = mossy_groups[tonode.name]
 				return grp and grp.mossy and tonode.name ~= grp.dying
 				and canbreathe(topos)
-				and nodecore.set_node(topos, {name = grp.mossy})
+				and setnodename(topos, tonode, grp.mossy)
 			end
 		end
 	})
@@ -177,32 +207,61 @@ minetest.register_abm({
 			if not canbreathe(pos) then
 				local grp = mossy_groups[node.name]
 				return grp and grp.original
-				and nodecore.set_node(pos, {name = grp.original})
+				and setnodename(pos, node, grp.original)
 			end
 			local found = nodecore.find_nodes_around(pos, "group:mossy", 1)
 			if #found < 1 then
 				local grp = mossy_groups[node.name]
 				return grp and grp.original
-				and nodecore.set_node(pos, {name = grp.original})
+				and setnodename(pos, node, grp.original)
 			end
 			local picked = found[math_random(1, #found)]
 			local tonode = minetest.get_node(picked)
 			local grp = mossy_groups[tonode.name]
-			return grp and grp.mossy and canbreathe(picked)
-			and nodecore.set_node(picked, {name = grp.dying})
+			return grp and grp.dying and canbreathe(picked)
+			and setnodename(picked, tonode, grp.dying)
 		end
 	})
-------------------------------------------------------------------------
- -- Hardstone Degradation --
-minetest.register_alias(modname.. ":mossy_hstone0",	modname.. ":mossy_stone")
-for i = 1,7 do
-nodecore.register_limited_abm({
-		label = "moss soften stone",
-		nodenames = {modname.. ":mossy_hstone"..i},
-		interval = 100,
-		chance = 10,
-		action = function(pos)
-				nodecore.set_node(pos, {name = modname .. ":mossy_hstone"..i-1})
+
+minetest.register_chatcommand("moss_blight", {
+		description = "Blight all nearby moss",
+		privs = {server = true},
+		params = "[radius]",
+		func = function(pname, param)
+			local player = minetest.get_player_by_name(pname)
+			if not player then return end
+			local pos = player:get_pos()
+			param = tonumber(param) or 5
+			if param < 0 then param = 0 end
+			if param > 79 then param = 79 end
+			for _, p in pairs(nodecore.find_nodes_around(pos, "group:mossy", param)) do
+				local node = minetest.get_node(p)
+				local grp = mossy_groups[node.name]
+				if grp and grp.dying then
+					setnodename(p, node, grp.dying)
+				end
+			end
 		end
 	})
-end
+minetest.register_chatcommand("moss_remove", {
+		description = "Remove all nearby moss",
+		privs = {server = true},
+		params = "[radius]",
+		func = function(pname, param)
+			local player = minetest.get_player_by_name(pname)
+			if not player then return end
+			local pos = player:get_pos()
+			param = tonumber(param) or 5
+			if param < 0 then param = 0 end
+			if param > 79 then param = 79 end
+			for _, p in pairs(nodecore.find_nodes_around(pos, {
+						"group:mossy", "group:mossy_dying"
+					}, param)) do
+				local node = minetest.get_node(p)
+				local grp = mossy_groups[node.name]
+				if grp and grp.original then
+					setnodename(p, node, grp.original)
+				end
+			end
+		end
+	})
